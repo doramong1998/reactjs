7bb2fb07fcfc552bb58e4b3dd2982041f46b71f7
doramong1998 <34101220+doramong1998@users.noreply.github.com>
2019-10-05 19:25:04 +0700

    call-api

---
+++ build/precache-manifest.749c2bbc31a93c809786e7b8af092933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c2f5cf154168aabe75b1f675c6d2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58935f2071a4164ede4",
+    "url": "/static/css/main.8390c35f.chunk.css"
+  },
+  {
+    "revision": "6b6d15796e496d77a09d",
+    "url": "/static/js/2.87b0e052.chunk.js"
+  },
+  {
+    "revision": "b58935f2071a4164ede4",
+    "url": "/static/js/main.64aaffe4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
